0db1a6a9ce11d51ce60376a592508b7fbe9dfd1a
zl <64017990+zengli826@users.noreply.github.com>
2020-05-28 18:34:53 +0800

    完成了11收入管理代码

---
+++ app/src/main/java/cn/edu/scujcc/moneyapp/other/OutManageActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.scujcc.moneyapp.other;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.scujcc.moneyapp.R;
+
+public class
+OutManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out_manage);
+    }
+}
