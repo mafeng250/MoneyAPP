93cc6ae86cd5425e24e56c10eb7e7432a1ae3bb6
“Ma <3043098480@qq.com>
2020-05-28 20:06:33 +0800

    添加build

---
+++ app/src/main/java/cn/edu/scujcc/moneyapp/activity/InComeDetailActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.scujcc.moneyapp.activity;
+
+import android.os.Bundle;
+
+import cn.edu.scujcc.moneyapp.R;
+
+public class InComeDetailActivity extends APPCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_come_detail);
+    }
+}
